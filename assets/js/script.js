2810cf236bbc01b61b5d79cdb758f73c2e7decdb
Cameron <sicklecj@gmail.com>
2022-03-18 18:55:13 -0500

    code restructing

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,16 @@ var time = startTime
 var score = 0
 var intervalId
 
-var allQuestions = [Q1, Q2]
+var quizDescription = "This is a Quiz. Good Luck."
+
+var countdownEl = document.getElementById('countdown')
+var scoreEl = document.getElementById('score')
+
+var questionsEl = document.getElementById('questions')
+var answersEl = document.getElementById('answers')
+var answerEl = document.getElementById('answer')
+
+var allQuestions = [Q1, Q2, Q3]
 
 var Q1 = "Who would win in a fight?"
 var Q1Answers = ["lion", "tiger", "bear", "chicken"]
@@ -11,36 +20,32 @@ var Q1Answers = ["lion", "tiger", "bear", "chicken"]
 var Q2 = "What do you want to eat?"
 var Q2Answers = ["pizza", "hotdog", "salad", "apple"]
 
+var Q3 = "Which color do you like best?"
+var Q3Answers = ["red", "blue", "green", "pink"]
 
-var questionsEl = document.getElementById('questions')
-var answersEl = document.getElementById('answers')
-var scoreEl = document.getElementById('score')
-var countdownEl = document.getElementById('countdown')
-
-
-
-
-
-function createQuestions() {
-    for (i = 0; i < 1; i++) {
-        var h2 = document.createElement('h2')
-        h2.innerText = Q1
-        h2.classList.add('question')
-        questionsEl.appendChild(h2)
-        var li = document.createElement('li')
-        li.innerText = Q1Answers
-        li.classList.add('answer')
-        // li.addEventListener('click', handleAnswerClick)
-        answersEl.appendChild(li)
-    }
+function handleStartClick() {
+    console.log("get clicked")
 }
 
+function createStartPage() {
+    console.log("creating start page...")
 
+    var h2 = document.createElement('h2')
+    h2.innerText = quizDescription
+    h2.addEventListener('click', handleStartClick)
+    questionsEl.appendChild(h2)
+}
 
+function startTimer() {
+    console.log("starting timer...")
+
+}
 
 
 function startQuiz() {
-    createQuestions()
+    console.log("quiz starting...")
+    createStartPage()
+    startTimer()
 }
 
-startQuiz()
+startQuiz();
