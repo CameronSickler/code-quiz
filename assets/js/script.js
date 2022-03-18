adcf156b476594df9108c8b7acaf5194ba33fa61
Cameron <sicklecj@gmail.com>
2022-03-18 12:19:09 -0500

    restructure HTML CSS add Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var startTime = 30
+var time = startTime
+var score = 0
+var intervalId
+
+
+var questionsEl = document.getElementById('questions')
+var scoreEl = document.getElementById('score')
+var countdownEl = document.getElementById('countdown')
