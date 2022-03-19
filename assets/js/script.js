151968c12ce4a60dc911e1e96ea6c8b1dc07dd28
Cameron <sicklecj@gmail.com>
2022-03-19 15:25:00 -0500

    add startQuiz function and clearElements function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,57 +13,80 @@ var questionsEl = document.getElementById('questions')
 var answersEl = document.getElementById('answers')
 var answerEl = document.getElementById('answer')
 
+var clearH2 = document.getElementById("questions");
+var clearUl = document.getElementById("answers");
+var clearLi = document.getElementById("answer");
+
 var allQuestions = [Q1, Q2, Q3]
+var allAnswers = [Q1A, Q2A, Q3A]
 
 var Q1 = "Who would win in a fight?"
-var Q1Answers = ["lion", "tiger", "bear", "chicken"]
+var Q1A = ["lion", "tiger", "bear", "chicken"]
 
 var Q2 = "What do you want to eat?"
-var Q2Answers = ["pizza", "hotdog", "salad", "apple"]
+var Q2A = ["pizza", "hotdog", "salad", "apple"]
 
 var Q3 = "Which color do you like best?"
-var Q3Answers = ["red", "blue", "green", "pink"]
+var Q3A = ["red", "blue", "green", "pink"]
 
 
 // functions
 
-function startQuiz(question, answers) {
-    //replaces questions
-    console.log('quiz starts here')
-    var aEl = document.getElementById('questions')
-    var newEl = document.createElement('questions')
-    newEl.innerText = question
-    newEl.className = "questions"
-    aEl.parentNode.replaceChild(newEl, aEl)
+function clearElements() {
 
-    //insert answers
-    var bEl = document.getElementById('answer')
-    var newbEl = document.createElement('answer')
-    newbEl.innerText = Q1Answers[0]
-    bEl.parentNode.replaceChild(newbEl, bEl)
+    //replace the h2 Element in HTML with null
+    clearH2.innerHTML = '';
 
-    for (i = 0; i < answers.length; i++) {
-        // clear answers element
-        //create and append new answers element
-    }
+    //replace the Ul Element in HTML with null
+    clearUl.innerHTML = '';
+
+    //replace the li Element in HTML with null
+    clearLi.innerHTML = '';
 }
 
 function handleStartClick() {
     console.log("get clicked")
     //call functions with perameters
-    startQuiz(Q1, Q1Answers)
+    startQuiz(Q1, Q1A)
 }
 
-function createStartPage() {
+function startQuiz(allQuestions, allAnswers) {
+    //clear all pre-existing elements
+    clearElements();
+
+    console.log('quiz starts here')
+
+    // for (i = 0; i = allQuestions[i]; i++) {
+    //create an h2 heading that holds quiz question
+    var h2 = document.createElement('h2')
+    h2.innerText = allQuestions
+    questionsEl.appendChild(h2)
+    // }
+    for (i = 0; i < allAnswers.length; i++) {
+        var ul = document.createElement('ul')
+        ul.className = "answer"
+        ul.innerText = allAnswers[i]
+        questionsEl.appendChild(ul)
+    }
+}
+
+
+function startPage() {
+    //clear pre-exsting elements
+    // clearElements();
+
     console.log("creating start page...")
 
+    //create an h2 heading that displays quiz description
     var h2 = document.createElement('h2')
     h2.innerText = quizDescription
     questionsEl.appendChild(h2)
 
+    // create an ul element to house li elements
     var ul = document.createElement('ul')
     answersEl.appendChild(ul)
 
+    // create li element to act as a button
     var li = document.createElement('li')
     li.innerText = quizBtn
     li.className = "answer"
@@ -75,10 +98,4 @@ function startTimer() {
     console.log("starting timer...")
 }
 
-function loadPage() {
-    console.log("filling page with elements...")
-    createStartPage()
-    startTimer()
-}
-
-loadPage();
+startPage();
