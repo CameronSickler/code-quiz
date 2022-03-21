e81b42defa3001498ab633aa1e7b34ad974ef8e9
Cameron <sicklecj@gmail.com>
2022-03-21 12:14:47 -0500

    add score and question index to handlestartclick function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,13 @@ var time = startTime
 var score = 0
 var intervalId
 
-// timer is meant to start at the beginning of quiz and continue to run until 0, which stops the quiz.
-// the score is meant to add/subtract based upon correct and incorrect answers.
+//The following variable progresses user through the quiz
+var currentQuestionIndex = 0;
+
+//The following variable sets a loop maximum for generating quiz answer choices.
+//The value should be equal to the number of choices within all the choices arrays
+//inside the allQuestions Object.  
+var choicesPerQuizQuestion = 4;
 
 //Start page Quiz variables
 var quizDescription = "This is a Quiz. Good Luck."
@@ -27,8 +32,7 @@ var clearUl = document.getElementById("answerslist");
 var clearLi = document.getElementById("answer");
 var clearConf = document.getElementById("confirmation");
 
-//All Q&As set into arrays
-var currentQuestionIndex = 0;
+//Object that holds all questions and their answers in an array. 
 var allQuestions = [
     {
         question: "Who would win in a fight?",
@@ -36,85 +40,104 @@ var allQuestions = [
         choices: ["lion", "tiger", "bear", "chicken"],
     },
     {
-        // keep going for remaining Question content
-
-    }
+        question: "What do you want to eat?",
+        answer: "pizza",
+        choices: ["pizza", "cheese", "donut", "apple"],
+    },
+    {
+        question: "Which color do you like best?",
+        answer: "red",
+        choices: ["blue", "red", "green", "purple"],
+    },
 ]
-
-//Each question set as a variable and arrays of answers
-var Q1A = ["lion", "tiger", "bear", "chicken"]
-var Q1Correct = "chicken"
-
-var Q2 = "What do you want to eat?"
-var Q2A = ["pizza", "hotdog", "salad", "apple"]
-
-var Q3 = "Which color do you like best?"
-var Q3A = ["red", "blue", "green", "pink"]
-
 
 // functions
 
+console.log("Welcome to Cameron's quiz generator");
+
+// This function clears any currently displayed elements.
+// This function is used by other functions to clear old content 
+// prior to creating and displaying new content. 
 function clearElements() {
 
     //replace the h2 Element in HTML with null
     clearH2.innerHTML = '';
-
     //replace the Ul Element in HTML with null
     clearUl.innerHTML = '';
-
     //replace the li Element in HTML with null
     clearLi.innerHTML = '';
 }
 
+//This function starts a Timer at the start of a quiz. 
 function startTimer() {
     console.log("starting timer...")
+
 }
 
+//This function handles starting the quiz. 
 function handleStartClick() {
     console.log("start clicked")
     //call functions with perameters
-    startQuiz()
+    //call startTimer function
+    quizGenerator()
 }
 
-function handleAnswerClick() {
-
+//This function handles a choice being clicked during the quiz.
+//I call this function with 'event' in the paremeters so that the identity of the eventListener that was clicked
+//to run the function is passed along as useful information. With that info, this function can check if the 
+// eventListener that was clicked was assigned to a correct or incorrect answer based upon the innerText.
+function handleAnswerClick(event) {
+    console.log(event.target)
     console.log('ive been clicked')
-    //need to discuss logic here for how best to proceed
+    // need to discuss logic here for how best to proceed
+    if (event.target.innerText === allQuestions[0].answer) {
+        console.log('correct answer of ' + event.target.innerText + ' was picked');
+        score = score + 1;
+        currentQuestionIndex = currentQuestionIndex + 1;
+    } else {
+        console.log('incorrect answer picked of ' + ' was picked');
+        score = score - 1;
+        currentQuestionIndex = currentQuestionIndex + 1;
+    }
+    quizGenerator();
 }
 
-function startQuiz() {
+// This function creates elements and populates them with quiz content
+function quizGenerator() {
     //clear all pre-existing elements
     clearElements();
 
-    console.log('quiz starts here')
-
-    // for (i = 0; i = allQuestions[i]; i++) {
-    //create an h2 heading that holds quiz question
+    // create an h2 element to display questions or quiz description
     var h2 = document.createElement('h2')
-    // h2.innerText = allQuestions
+    // set the h2 element I created to look into the allQuestions object, go to the array item that is 
+    //equal to currentQuestionIndex at the moment, and display the value of the object property called 
+    // 'question' as the innerText element of this newly created h2 element. 
     h2.innerText = allQuestions[currentQuestionIndex].question
     questionsEl.appendChild(h2)
-    // }
 
     // create an ul element to house li elements
     var ul = document.createElement('ul')
     answersListEl.appendChild(ul)
 
-    console.log('created Ul success')
-    console.log(ul)
-
-    // make the conditional statement dynamic later
-    for (i = 0; i < 4; i++) {
+    // for loop here is going to generate li elements and populate them with choices. It loops until
+    // we have generated as many li elements as available choices in the allQuestions array. Each question
+    // has a total of 4 possible choices, so I made a global variable called choicesPerQuizQuestion and 
+    // set it to 4.
+    for (i = 0; i < choicesPerQuizQuestion; i++) {
         var li = document.createElement('li')
+        // sets the li innerText to be the an array item picked from the choices array
+        // as determined by the value of 'i' at the time it is called. The particular set of choices
+        //comes from the allQuestions object as determined by the value of currentQuestion Index at the 
+        //time the it is called. The global variable currentQuestionIndex is set to 0 as default.
         li.innerText = allQuestions[currentQuestionIndex].choices[i];
         li.className = "answer"
         li.addEventListener('click', handleAnswerClick)
         answersListEl.appendChild(li)
-        console.log(li)
     }
 
 }
 
+// This function starts the page the user first sees
 function startPage() {
     //clear pre-exsting elements
     clearElements();
@@ -138,6 +161,5 @@ function startPage() {
     answersListEl.appendChild(li)
 }
 
-
-
+// calls the startPage function
 startPage();
