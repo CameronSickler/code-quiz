8b3ab46d5c7d0fc900499fe7ce6046c688af0f62
Cameron <sicklecj@gmail.com>
2022-03-21 08:36:12 -0500

    combined arrays into object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,49 @@
+//Quiz timer & Score variables
 var startTime = 30
 var time = startTime
 var score = 0
 var intervalId
 
+// timer is meant to start at the beginning of quiz and continue to run until 0, which stops the quiz.
+// the score is meant to add/subtract based upon correct and incorrect answers.
+
+//Start page Quiz variables
 var quizDescription = "This is a Quiz. Good Luck."
 var quizBtn = "Click to Begin"
 
+//Correct! and Wrong! variables
 var countdownEl = document.getElementById('countdown')
 var scoreEl = document.getElementById('score')
 
+//Quiz questions and answers variables
 var questionsEl = document.getElementById('questions')
 var answersListEl = document.getElementById('answerslist')
 var answerEl = document.getElementById('answer')
+var confirmEl = document.getElementById('confirmation')
 
+//Variables used in the clearElements Function
 var clearH2 = document.getElementById("questions");
 var clearUl = document.getElementById("answerslist");
 var clearLi = document.getElementById("answer");
+var clearConf = document.getElementById("confirmation");
 
-var allQuestions = [Q1, Q2, Q3]
-var allAnswers = [Q1A, Q2A, Q3A]
+//All Q&As set into arrays
+var currentQuestionIndex = 0;
+var allQuestions = [
+    {
+        question: "Who would win in a fight?",
+        answer: "chicken",
+        choices: ["lion", "tiger", "bear", "chicken"],
+    },
+    {
+        // keep going for remaining Question content
 
-var Q1 = "Who would win in a fight?"
+    }
+]
+
+//Each question set as a variable and arrays of answers
 var Q1A = ["lion", "tiger", "bear", "chicken"]
+var Q1Correct = "chicken"
 
 var Q2 = "What do you want to eat?"
 var Q2A = ["pizza", "hotdog", "salad", "apple"]
@@ -44,19 +66,23 @@ function clearElements() {
     clearLi.innerHTML = '';
 }
 
+function startTimer() {
+    console.log("starting timer...")
+}
+
 function handleStartClick() {
     console.log("start clicked")
     //call functions with perameters
-    startQuiz(Q1, Q1A)
+    startQuiz()
 }
 
 function handleAnswerClick() {
-    console.log('answer clicked')
-    //if its question 1 and correct answer run q2 function
-    //if its question 1 and wrong answer run end quiz function
+
+    console.log('ive been clicked')
+    //need to discuss logic here for how best to proceed
 }
 
-function startQuiz(allQuestions, allAnswers) {
+function startQuiz() {
     //clear all pre-existing elements
     clearElements();
 
@@ -65,7 +91,8 @@ function startQuiz(allQuestions, allAnswers) {
     // for (i = 0; i = allQuestions[i]; i++) {
     //create an h2 heading that holds quiz question
     var h2 = document.createElement('h2')
-    h2.innerText = allQuestions
+    // h2.innerText = allQuestions
+    h2.innerText = allQuestions[currentQuestionIndex].question
     questionsEl.appendChild(h2)
     // }
 
@@ -76,9 +103,10 @@ function startQuiz(allQuestions, allAnswers) {
     console.log('created Ul success')
     console.log(ul)
 
-    for (i = 0; i < allAnswers.length; i++) {
+    // make the conditional statement dynamic later
+    for (i = 0; i < 4; i++) {
         var li = document.createElement('li')
-        li.innerText = allAnswers[i]
+        li.innerText = allQuestions[currentQuestionIndex].choices[i];
         li.className = "answer"
         li.addEventListener('click', handleAnswerClick)
         answersListEl.appendChild(li)
@@ -86,7 +114,6 @@ function startQuiz(allQuestions, allAnswers) {
     }
 
 }
-
 
 function startPage() {
     //clear pre-exsting elements
@@ -111,8 +138,6 @@ function startPage() {
     answersListEl.appendChild(li)
 }
 
-function startTimer() {
-    console.log("starting timer...")
-}
+
 
 startPage();
